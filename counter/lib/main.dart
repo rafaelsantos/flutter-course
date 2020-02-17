032e814e9879454c2206bca57893b444b608f9c0
rafaelsantos <rafael.balieiro@luizalabs.com>
2020-02-17 13:34:52 -0300

    Adding columns widget and text widget

--- counter/lib/main.dart
+++ counter/lib/main.dart
@@ -2,7 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "People Counter",
-    home: Container(color: Colors.white,)
-  ));
+      title: "People Counter",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "People: 0",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Available",
+            style: TextStyle(color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0),
+          )
+        ],
+      )));
 }
