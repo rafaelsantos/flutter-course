6ece43fc8f43c04b18fc1abfabd9cc2dfb74313a
rafaelsantos <rafael.balieiro@luizalabs.com>
2020-02-21 13:14:32 -0300

    Working with future builder

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -34,6 +34,37 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.amber,
         centerTitle: true,
       ),
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch(snapshot.connectionState){
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text("Loading data...",
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25.0
+                    ),
+                    textAlign: TextAlign.center,
+                  )
+                );
+              default:
+                if(snapshot.hasError){
+                  return Center(
+                      child: Text("Error on loading data!",
+                        style: TextStyle(
+                            color: Colors.amber,
+                            fontSize: 25.0
+                        ),
+                        textAlign: TextAlign.center,
+                      )
+                  );
+                } else {
+                  return Container(color: Colors.green);
+                }
+            }
+          })
     );
   }
 }
