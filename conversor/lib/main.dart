a238865de53269dcb571f8fd628676ee30190b0f
rafaelsantos <rafael.balieiro@luizalabs.com>
2020-02-21 13:01:06 -0300

    Working with future maps

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=e65f6855";
 
 void main() async {
 
-  http.Response response = await http.get(request);
-  print(json.decode(response.body));
-  print(json.decode(response.body)["results"]);
-  print(json.decode(response.body)["results"]["currencies"]);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+  print(await getData());
 
   runApp(MaterialApp(
-    home: Container()
+    home: Home()
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
 }
