3adab9e24f5b8fb53b3d3c3352c92ec7308324b9
rafaelsantos <rafael.balieiro@luizalabs.com>
2020-02-28 19:40:24 -0300

    Implementing sorting in intem list

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -46,6 +46,22 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _todo.sort((a, b){
+        if(a["ok"] && !b["ok"]) return 1;
+        else if(!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+
+      _saveData();
+    });
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,10 +94,14 @@ class _HomeState extends State<Home> {
               ),
             ),
             Expanded(
-                child: ListView.builder(
-                    padding: EdgeInsets.only(top: 10.0),
-                    itemCount: _todo.length,
-                    itemBuilder: buildItem))
+                child: RefreshIndicator(
+                  onRefresh: _refresh,
+                  child: ListView.builder(
+                      padding: EdgeInsets.only(top: 10.0),
+                      itemCount: _todo.length,
+                      itemBuilder: buildItem),
+                )
+            )
           ],
         ));
   }
@@ -127,7 +147,8 @@ class _HomeState extends State<Home> {
             ),
             duration: Duration(seconds: 5),
           );
-          
+
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
       },
