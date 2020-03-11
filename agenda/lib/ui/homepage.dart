bb7c64d83b11444bd880cd9e7ef25b4724651ea3
rafaelsantos <rafael.balieiro@luizalabs.com>
2020-03-11 12:44:47 -0300

    Creating contact card list

--- agenda/lib/ui/homepage.dart
+++ agenda/lib/ui/homepage.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:agenda/helpers/contactHelper.dart';
 import 'package:flutter/material.dart';
 
@@ -42,9 +44,52 @@ class _HomePageState extends State<HomePage> {
           padding: EdgeInsets.all(10.0),
           itemCount: contacts.length,
           itemBuilder: (context, index) {
-            return null;
+            return _contactCard(context, index);
           }
       ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: contacts[index].image != null ?
+                          FileImage(File(contacts[index].image)) :
+                          AssetImage("images/person.png")
+                  )
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(contacts[index].name ?? "",
+                     style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+                    ),
+                    Text(contacts[index].mail ?? "",
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    Text(contacts[index].phone ?? "",
+                      style: TextStyle(fontSize: 18.0),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        )
+      )
     );
   }
 }
