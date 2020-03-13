7ddda6ee50971d88a35c2c3e6980642803ad11e7
rafaelsantos <rafael.balieiro@luizalabs.com>
2020-03-13 12:59:54 -0300

    Adding alert dialog for unsaved changes

--- agenda/lib/ui/contactpage.dart
+++ agenda/lib/ui/contactpage.dart
@@ -41,69 +41,104 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text(_editedContact.name ?? "New Contact"),
-          centerTitle: true,
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            if(_editedContact.name != null && _editedContact.name.isNotEmpty) {
-              Navigator.pop(context, _editedContact);
-            } else {
-              FocusScope.of(context).requestFocus(_nameFocus);
-            }
-          },
-          child: Icon(Icons.save),
-          backgroundColor: Colors.red,
-        ),
-        body: SingleChildScrollView(
-          padding: EdgeInsets.all(10.0),
-          child: Column(
-            children: <Widget>[
-              GestureDetector(
-                child: Container(
-                  width: 140.0,
-                  height: 140.0,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: _editedContact.image != null
-                              ? FileImage(File(_editedContact.image))
-                              : AssetImage("images/person.png"))),
-                ),
-              ),
-              TextField(
-                controller: _nameController,
-                focusNode: _nameFocus,
-                decoration: InputDecoration(labelText: "Name"),
-                onChanged: (text){
-                  _userEdited = true;
-                  setState(() {
-                    _editedContact.name = text;
-                  });
-                },
-              ),
-              TextField(
-                controller: _mailController,
-                decoration: InputDecoration(labelText: "Mail"),
-                onChanged: (text){
-                  _userEdited = true;
-                  _editedContact.mail = text;
-                },
-                keyboardType: TextInputType.emailAddress,
-              ),
-              TextField(
-                controller: _phoneController,
-                decoration: InputDecoration(labelText: "Phone"),
-                onChanged: (text){
-                  _userEdited = true;
-                  _editedContact.phone = text;
-                }, keyboardType: TextInputType.phone
-              )
-            ],
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text(_editedContact.name ?? "New Contact"),
+            centerTitle: true,
           ),
-        ));
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              if(_editedContact.name != null && _editedContact.name.isNotEmpty) {
+                Navigator.pop(context, _editedContact);
+              } else {
+                FocusScope.of(context).requestFocus(_nameFocus);
+              }
+            },
+            child: Icon(Icons.save),
+            backgroundColor: Colors.red,
+          ),
+          body: SingleChildScrollView(
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                GestureDetector(
+                  child: Container(
+                    width: 140.0,
+                    height: 140.0,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: _editedContact.image != null
+                                ? FileImage(File(_editedContact.image))
+                                : AssetImage("images/person.png"))),
+                  ),
+                ),
+                TextField(
+                  controller: _nameController,
+                  focusNode: _nameFocus,
+                  decoration: InputDecoration(labelText: "Name"),
+                  onChanged: (text){
+                    _userEdited = true;
+                    setState(() {
+                      _editedContact.name = text;
+                    });
+                  },
+                ),
+                TextField(
+                  controller: _mailController,
+                  decoration: InputDecoration(labelText: "Mail"),
+                  onChanged: (text){
+                    _userEdited = true;
+                    _editedContact.mail = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                TextField(
+                    controller: _phoneController,
+                    decoration: InputDecoration(labelText: "Phone"),
+                    onChanged: (text){
+                      _userEdited = true;
+                      _editedContact.phone = text;
+                    }, keyboardType: TextInputType.phone
+                )
+              ],
+            ),
+          ))
+    );
+  }
+
+  Future<bool> _requestPop() {
+    if(_userEdited) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("Discard changes?"),
+              content: Text("Changes will be lost"),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text("Cancel"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                FlatButton(
+                    child: Text("Yes"),
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                    }
+                )
+              ],
+            );
+          }
+      );
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
   }
 }
