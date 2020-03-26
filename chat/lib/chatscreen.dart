7bbdd2928e7ade4d5b912d8fcc0a4a3f98bf98e5
rafaelsantos <rafael.balieiro@luizalabs.com>
2020-03-26 07:47:16 -0300

    Creating layout adding stateful widget text composer

---
+++ chat/lib/chatscreen.dart
@@ -0,0 +1,20 @@
+import 'package:chat/textcomposer.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+        elevation: 0,
+      ),
+      body: TextComposer(),
+    );
+  }
+}
--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -1,28 +1,9 @@
+import 'package:chat/chatscreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   runApp(MyApp());
-
-  QuerySnapshot snapshot = await Firestore.instance.collection("messages").getDocuments();
-
-  snapshot.documents.forEach((d){
-    d.reference.updateData({"read": false});
-
-    print(d.data);
-    print(d.documentID);
-  });
-
-  DocumentSnapshot dSnapshot = await Firestore.instance.collection("messages")
-    .document("a7MtgeStq3vcttk2PUC8").get();
-
-  print(dSnapshot.data);
-
-  Firestore.instance.collection("messages").snapshots().listen((data){
-    data.documents.forEach((value){
-      print(value.data);
-    });
-  });
 }
 
 class MyApp extends StatelessWidget {
@@ -33,8 +14,11 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue,
+        )
       ),
-      home: Container(),
+      home: ChatScreen(),
     );
   }
 }
