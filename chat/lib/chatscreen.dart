0632d5545c45f44695ce3709056f8c57c1c13017
rafaelsantos <santos.rafaelbs@gmail.com>
2020-05-19 20:53:49 -0300

    Implementing feature to show details of user in messages

--- chat/lib/chatscreen.dart
+++ chat/lib/chatscreen.dart
@@ -7,6 +7,8 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import 'chatmessage.dart';
+
 class ChatScreen extends StatefulWidget {
   @override
   _ChatScreenState createState() => _ChatScreenState();
@@ -45,6 +47,7 @@ class _ChatScreenState extends State<ChatScreen> {
       final FirebaseUser user = authResult.user;
       return user;
     } catch(error) {
+      print(error);
       return null;
     }
   }
@@ -113,9 +116,7 @@ class _ChatScreenState extends State<ChatScreen> {
                         itemCount: documents.length,
                         reverse: true,
                         itemBuilder: (context, index) {
-                          return ListTile(
-                            title: Text(documents[index].data['text']),
-                          );
+                          return ChatMessage(documents[index].data, true);
                         }
                     );
                 }
