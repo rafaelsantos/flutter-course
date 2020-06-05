25aa209d0efd7f3dbb93390ae407b4d6d72b28b3
rafaelsantos <santos.rafaelbs@gmail.com>
2020-06-05 11:15:14 -0300

    Creating project app store

---
+++ store/store/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop Store',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Container()
+    );
+  }
+}
