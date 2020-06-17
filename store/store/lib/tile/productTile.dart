86d871313f1fe4a5c39487f4eee05071a430e53b
rafaelsantos <santos.rafaelbs@gmail.com>
2020-06-17 13:24:17 -0300

    Implementing list and grid widgets

---
+++ store/store/lib/tile/productTile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:store/data/productData.dart';
+
+class ProductTile extends StatelessWidget {
+
+  final String type;
+  final ProductData data;
+
+  ProductTile(this.type, this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- store/store/lib/ui/categoryUI.dart
+++ store/store/lib/ui/categoryUI.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:store/data/productData.dart';
+import 'package:store/tile/productTile.dart';
 
 class CategoryUI extends StatelessWidget {
 
@@ -23,12 +25,38 @@ class CategoryUI extends StatelessWidget {
             ],
           ),
         ),
-        body: TabBarView(
-          physics: NeverScrollableScrollPhysics(),
-          children: <Widget>[
-            Container(color: Colors.red),
-            Container(color: Colors.green)
-          ],
+        body: FutureBuilder<QuerySnapshot>(
+          future: Firestore.instance.collection("products").document(snapshot.documentID)
+          .collection("items").getDocuments(),
+          builder: (context, snapshot){
+            if(!snapshot.hasData)
+              return Center(child: CircularProgressIndicator());
+            return TabBarView(
+              physics: NeverScrollableScrollPhysics(),
+              children: <Widget>[
+                GridView.builder(
+                  padding: EdgeInsets.all(4.0),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 4.0,
+                      crossAxisSpacing: 4.0,
+                      childAspectRatio: 0.65
+                    ),
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index){
+                      return ProductTile("grid", ProductData.fromDocument(snapshot.data.documents[index]));
+                    }
+                ),
+                ListView.builder(
+                    padding: EdgeInsets.all(4.0),
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, index){
+                      return ProductTile("list", ProductData.fromDocument(snapshot.data.documents[index]));
+                    }
+                )
+              ],
+            );
+          },
         )
       )
     );
