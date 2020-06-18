722b92a266c3a0225b869a28b538a414243a0b8e
rafaelsantos <santos.rafaelbs@gmail.com>
2020-06-18 13:14:17 -0300

    Implementing grid tile

--- store/store/lib/tile/productTile.dart
+++ store/store/lib/tile/productTile.dart
@@ -10,6 +10,47 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //Could be inkwell or gesture detector, but inkwell will create a animation
+    return InkWell(
+      child: Card(
+        child: type == "grid" ?
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 0.8,
+                child: Image.network(
+                  data.images[0],
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(data.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500
+                        ),
+                      ),
+                      Text(
+                        "R\$ ${data.price.toStringAsFixed(2)}",
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+        : Row(),
+      ),
+    );
   }
 }
